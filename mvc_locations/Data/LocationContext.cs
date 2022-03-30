39b7860db5f05cad8ef760d016af5cc47283b07d
borojevicfilip@gmail.com <borojevicfilip@gmail.com>
2022-03-30 19:07:12 +0200

    Add project files.

---
+++ mvc_locations/Data/LocationContext.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore;
+using mvc_locations.Models;
+
+namespace mvc_locations.Data
+{
+    public class LocationContext : DbContext
+    {
+
+        public LocationContext(DbContextOptions<LocationContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        // Zadatak:
+        // kreirati metodu OnModelCreating(ModelBuilder modelBuilder)
+        // Napraviti seed podataka za:
+        // Minimalno dvije nasumične države s pripadajućim podacima
+        // minimalno šest nasumičnih gradova s pripadajućim podacima
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Country>().HasData(
+                new Country
+                {
+                    id = 1,
+                    country_name = "Danska",
+                    country_name_eng =  "Denmark",
+                    country_code = "+45"
+                },
+                new Country
+                {
+                    id = 2,
+                    country_name = "Španjolska",
+                    country_name_eng = "Spian",
+                    country_code = "+34"
+                }
+            );
+
+
+            modelBuilder.Entity<City>().HasData(
+                new City
+                {
+                    id = 1,
+                    city_name = "Kopenhagen",
+                    country_id = 1,
+                    latitude = 55.672520m,
+                    longitude = 12.563413m
+
+                },
+                new City
+                {
+                    id = 2,
+                    city_name = "Holstebro",
+                    country_id = 1,
+                    latitude = 56.362564m,
+                    longitude = 8.621231m
+                },
+                new City
+                {
+                    id = 3,
+                    city_name = "Kolding",
+                    country_id = 1,
+                    latitude = 55.495972m,
+                    longitude = 9.473052m
+                },
+                new City
+                {
+                    id = 4,
+                    city_name = "Madrid",
+                    country_id = 2,
+                    latitude = 40.416775m,
+                    longitude = -3.703790m
+                },
+                new City
+                {
+                    id = 5,
+                    city_name = "Barcelona",
+                    country_id = 2,
+                    latitude = 41.346176m,
+                    longitude = 2.168365m
+                },
+                new City
+                {
+                    id = 6,
+                    city_name = "Sevilla",
+                    country_id = 2,
+                    latitude = 37.392529m,
+                    longitude = -5.994072m
+                }
+
+            );
+        }
+
+
+
+
+
+
+    }
+}
---
+++ mvc_locations/Migrations/20220330170121_NewData.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mvc_locations.Migrations
+{
+    public partial class NewData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "id", "country_code", "country_name", "country_name_eng" },
+                values: new object[] { 1, "+45", "Danska", "Denmark" });
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "id", "country_code", "country_name", "country_name_eng" },
+                values: new object[] { 2, "+34", "Španjolska", "Spian" });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "id", "city_name", "country_id", "latitude", "longitude" },
+                values: new object[,]
+                {
+                    { 1, "Kopenhagen", 1, 55.672520m, 12.563413m },
+                    { 2, "Holstebro", 1, 56.362564m, 8.621231m },
+                    { 3, "Kolding", 1, 55.495972m, 9.473052m },
+                    { 4, "Madrid", 2, 40.416775m, -3.703790m },
+                    { 5, "Barcelona", 2, 41.346176m, 2.168365m },
+                    { 6, "Sevilla", 2, 37.392529m, -5.994072m }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "id",
+                keyValue: 2);
+        }
+    }
+}
